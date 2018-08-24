0b834132bf1f4355bf4a5eaa5515f68ad5e09910
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-08-24 06:43:56 +0200

    Minor cleanup

--- org.moflon.tie_gt.ide/src/org/moflon/tie/gt/ide/core/patterns/PatternBuilderVisitor.java
+++ org.moflon.tie_gt.ide/src/org/moflon/tie/gt/ide/core/patterns/PatternBuilderVisitor.java
@@ -11,7 +11,6 @@ import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EEnumLiteral;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.impl.EStructuralFeatureImpl.SimpleContentFeatureMapEntry;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.emoflon.ibex.gt.editor.gT.EditorApplicationCondition;
 import org.emoflon.ibex.gt.editor.gT.EditorApplicationConditionType;
@@ -29,7 +28,6 @@ import org.emoflon.ibex.gt.editor.gT.EditorParameterExpression;
 import org.emoflon.ibex.gt.editor.gT.EditorPattern;
 import org.emoflon.ibex.gt.editor.gT.EditorReference;
 import org.emoflon.ibex.gt.editor.gT.EditorRelation;
-import org.emoflon.ibex.gt.editor.gT.EditorSimpleCondition;
 import org.emoflon.ibex.gt.editor.utils.GTEditorAttributeUtils;
 import org.gervarro.democles.specification.emf.Constant;
 import org.gervarro.democles.specification.emf.Constraint;
@@ -126,7 +124,7 @@ public class PatternBuilderVisitor {
 						break;
 				}
 			}
-			
+
 		});
 	}
 
