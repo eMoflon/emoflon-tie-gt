aa7c9d57b55b7cf36f417d6fc58bb8042d0d7435
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-12-17 08:18:14 +0100

    Remove unused class

--- org.moflon.sdm.compiler.eclipse/src/org/moflon/codegen/eclipse/CodeGeneratorPlugin.java
+++
@@ -1,8 +0,0 @@
-package org.moflon.codegen.eclipse;
-
-import org.eclipse.core.runtime.Plugin;
-
-@Deprecated //Since 2018-03-21
-public class CodeGeneratorPlugin extends Plugin {
-	// Currently unused
-}
