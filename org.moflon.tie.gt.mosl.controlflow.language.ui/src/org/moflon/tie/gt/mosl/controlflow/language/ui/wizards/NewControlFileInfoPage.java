c57f2700b2d40f7bc7606a572e1af02f3712029b
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2019-01-18 10:47:43 +0100

    Enforce file extension mcf

--- org.moflon.tie.gt.mosl.controlflow.language.ui/src/org/moflon/tie/gt/mosl/controlflow/language/ui/wizards/NewControlFileInfoPage.java
+++ org.moflon.tie.gt.mosl.controlflow.language.ui/src/org/moflon/tie/gt/mosl/controlflow/language/ui/wizards/NewControlFileInfoPage.java
@@ -1,14 +1,14 @@
 package org.moflon.tie.gt.mosl.controlflow.language.ui.wizards;
 
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.moflon.tie.gt.mosl.controlflow.language.ui.wizards.ControlFileTemplate;
 import org.moflon.tie.gt.mosl.ide.ui.wizards.AbstractNewFileInfoPage;
 import org.moflon.tie.gt.mosl.ide.ui.wizards.WizardFileTemplate;
 
 public class NewControlFileInfoPage extends AbstractNewFileInfoPage {
 
-	public NewControlFileInfoPage(IStructuredSelection selection) {
+	public NewControlFileInfoPage(final IStructuredSelection selection) {
 		super("New eMoflon::TIE-GT Controlflow Specification File", selection, "mcf");
+		setFileExtension("mcf");
 	}
 
 	@Override
