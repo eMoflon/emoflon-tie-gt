8009dfd1ba7546ad9c97a192f462a618c61a1ba5
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2019-01-21 07:47:42 +0100

    Remove unused imports

--- org.moflon.tie.gt.mosl.controlflow.language/src/org/moflon/tie/gt/mosl/controlflow/language/utils/ControlFlowEditorModelUtil.java
+++ org.moflon.tie.gt.mosl.controlflow.language/src/org/moflon/tie/gt/mosl/controlflow/language/utils/ControlFlowEditorModelUtil.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EDataType;
@@ -17,16 +16,11 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.EcoreUtil2;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.resource.XtextResourceSet;
 import org.moflon.core.utilities.eMoflonEMFUtil;
 import org.moflon.tie.gt.mosl.controlflow.language.moslControlFlow.GraphTransformationControlFlowFile;
 
-import com.google.inject.Injector;
-
 public class ControlFlowEditorModelUtil {
 
-	
 	/**
 	 * The set of meta-model resources loaded.
 	 */
@@ -40,8 +34,7 @@ public class ControlFlowEditorModelUtil {
 	/**
 	 * Returns all EClasses imported into the given file.
 	 * 
-	 * @param file
-	 *            the GT file
+	 * @param file the GT file
 	 */
 	public static ArrayList<EClass> getClasses(final GraphTransformationControlFlowFile file) {
 		final ArrayList<EClass> classes = new ArrayList<EClass>();
@@ -54,8 +47,7 @@ public class ControlFlowEditorModelUtil {
 	/**
 	 * Returns all EDataTypes imported into the given file.
 	 * 
-	 * @param file
-	 *            the GT file
+	 * @param file the GT file
 	 */
 	public static ArrayList<EDataType> getDatatypes(final GraphTransformationControlFlowFile file) {
 		final ArrayList<EDataType> types = new ArrayList<EDataType>();
@@ -68,10 +60,8 @@ public class ControlFlowEditorModelUtil {
 	/**
 	 * Returns all objects from the given resource.
 	 * 
-	 * @param resource
-	 *            the resource
-	 * @param type
-	 *            the type
+	 * @param resource the resource
+	 * @param type     the type
 	 * @return the elements with the given type in the resource
 	 */
 	public static <T extends EObject> List<T> getElements(final Resource resource, final Class<T> type) {
@@ -81,16 +71,15 @@ public class ControlFlowEditorModelUtil {
 	/**
 	 * Returns an Optional for the Ecore model resource with the given URI.
 	 * 
-	 * @param uriString
-	 *            the URI of the resource to load
+	 * @param uriString the URI of the resource to load
 	 */
 	public static Optional<Resource> loadEcoreModel(final String uriString) {
-		URI uri = URI.createURI(uriString);
+		final URI uri = URI.createURI(uriString);
 		try {
 			final ResourceSet resourceset = new ResourceSetImpl();
 			eMoflonEMFUtil.createPluginToResourceMapping(resourceSet);
-			//Resource resource=eMoflonEMFUtil.loadModel(uri, resourceset).eResource();
-			final Resource resource=resourceSet.getResource(uri, true);
+			// Resource resource=eMoflonEMFUtil.loadModel(uri, resourceset).eResource();
+			final Resource resource = resourceSet.getResource(uri, true);
 			resource.load(null);
 
 			// Early return if the resource does not exist or is empty.
@@ -115,13 +104,12 @@ public class ControlFlowEditorModelUtil {
 	 * Removes the resource for the given URI from the resource set and the mapping
 	 * between URIs and resources.
 	 * 
-	 * @param uri
-	 *            the URI to remove
+	 * @param uri the URI to remove
 	 * @return an empty optional
 	 */
 	private static void removeResource(final URI uri) {
 		if (metaModelResources.containsKey(uri)) {
-			Resource resource = metaModelResources.get(uri);
+			final Resource resource = metaModelResources.get(uri);
 			resource.unload();
 			resourceSet.getResources().remove(resource);
 			metaModelResources.remove(uri);
@@ -132,29 +120,25 @@ public class ControlFlowEditorModelUtil {
 	 * Updates the resource for the given URI in the resource set and the mapping
 	 * between URIs and resources.
 	 * 
-	 * @param uri
-	 *            the URI to update
-	 * @throws IOException
-	 *             if the resource cannot be loaded
+	 * @param uri the URI to update
+	 * @throws IOException if the resource cannot be loaded
 	 */
 	private static void updateResource(final URI uri) throws IOException {
 		// Remove resource if it was loaded before -> force reload.
 		removeResource(uri);
 
-		Resource resource = resourceSet.getResource(uri, true);
+		final Resource resource = resourceSet.getResource(uri, true);
 		resource.load(null);
 		EcoreUtil.resolveAll(resourceSet);
 		metaModelResources.put(uri, resource);
 	}
-	
+
 	private static ResourceSetImpl createNewXtextEnabledResourceSet() {
-			/*eMoflonEMFUtil.installCrossReferencers(resourceSet);
-			try {
-				eMoflonEMFUtil.createPluginToResourceMapping(resourceSet);
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
-			return new ResourceSetImpl();
+		/*
+		 * eMoflonEMFUtil.installCrossReferencers(resourceSet); try {
+		 * eMoflonEMFUtil.createPluginToResourceMapping(resourceSet); } catch
+		 * (CoreException e) { // TODO Auto-generated catch block e.printStackTrace(); }
+		 */
+		return new ResourceSetImpl();
 	}
 }
